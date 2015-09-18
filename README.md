7f2411364b3508c416410bc3ec46b8242ae34c09
Boyer, Shayne P <Shayne.P.Boyer@disney.com>
2015-09-18 12:36:40 -0400

    update to new

---
+++ README.md
@@ -0,0 +1,19 @@
+# [Ghost (0.7.0)](https://github.com/TryGhost/Ghost) [![Deploy to Azure](http://azuredeploy.net/deploybutton.png)](https://azuredeploy.net/)
+
+Want to install Ghost on Azure? Just click the 'Deploy to Azure' button and you're well on your way to have a Ghost Blog running within a minute or two. Made with :heart: for Ghost by Microsoft DX.
+
+**Note: After deployment, Ghost will continue setup - resulting in your brand new website staying blank on the very first load. Just refresh, you'll see that everything worked well!**
+
+#### Custom Domain
+Directly after installation, Ghost will assume that your blog runs at `http://sitename.azurewebsites.net`. Should you switch to a custom domain, please inform Ghost about that change - you can do by setting the App Setting `websiteUrl` and restarting your website.
+
+## Ghost
+![Ghost Screenshot](https://cloud.githubusercontent.com/assets/120485/4828504/9e832764-5f80-11e4-8ac1-0332bcc67a35.png)
+
+Ghost is a free, open, simple blogging platform that's available to anyone who wants to use it. Lovingly created and maintained by [John O'Nolan](http://twitter.com/JohnONolan) + [Hannah Wolfe](http://twitter.com/ErisDS) + an amazing group of [contributors](https://github.com/TryGhost/Ghost/contributors).
+
+Visit the project's website at <http://ghost.org> &bull; docs on <http://support.ghost.org>.
+
+## Copyright & License
+
+Ghost is Copyright (c) 2013-2015 Ghost Foundation - Released under the [MIT license](LICENSE).
---
+++ config.js
@@ -0,0 +1,152 @@
+// # Ghost Configuration for Azure Deployment
+// Setup your Ghost install for various environments
+// Documentation can be found at http://support.ghost.org/config/
+
+var path = require('path'),
+    websiteUrl = process.env.websiteUrl,
+    config;
+
+// Azure Feature
+// ------------------------------------------------------------------------
+// If the App Setting 'websiteUrl' is set, Ghost will use that URL as base.
+// If it isn't set, we'll go with the default sitename.
+if (!websiteUrl || websiteUrl === '' ||  websiteUrl.length === 0) {
+    websiteUrl = 'http://' + process.env.siteName + '.azurewebsites.net';
+    console.log(websiteUrl);
+}
+
+config = {
+    // ### Development **(default)**
+    development: {
+        // The url to use when providing links to the site, E.g. in RSS and email.
+        url: websiteUrl,
+
+        // Visit http://support.ghost.org/mail for instructions
+         mail: {
+             transport: 'SMTP',
+             options: {
+                 service: process.env.emailService,
+                 auth: {
+                     user: process.env.emailUsername, // mailgun username
+                     pass: process.env.emailPassword  // mailgun password
+                 }
+             }
+         },
+
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-dev.db')
+            },
+            debug: false
+        },
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '127.0.0.1',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: process.env.PORT
+        },
+        paths: {
+            contentPath: path.join(__dirname, '/content/')
+        },
+        forceAdminSSL: false
+    },
+
+    // ### Production
+    // When running Ghost in the wild, use the production environment
+    // Configure your URL and mail settings here
+    production: {
+        url: websiteUrl,
+
+        // Visit http://support.ghost.org/mail for instructions
+        mail: {
+         transport: 'SMTP',
+         options: {
+             service: process.env.emailService,
+             auth: {
+                 user: process.env.emailUsername, // mailgun username
+                 pass: process.env.emailPassword  // mailgun password
+             }
+         }
+        },
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost.db')
+            },
+            debug: false
+        },
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '127.0.0.1',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: process.env.PORT
+        },
+        forceAdminSSL: true
+    },
+
+    // **Developers only need to edit below here**
+
+    // ### Testing
+    // Used when developing Ghost to run tests and check the health of Ghost
+    // Uses a different port number
+    testing: {
+        url: 'http://127.0.0.1:2369',
+        database: {
+            client: 'sqlite3',
+            connection: {
+                filename: path.join(__dirname, '/content/data/ghost-test.db')
+            }
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '2369'
+        },
+        logging: false
+    },
+
+    // ### Testing MySQL
+    // Used by Travis - Automated testing run through GitHub
+    'testing-mysql': {
+        url: 'http://127.0.0.1:2369',
+        database: {
+            client: 'mysql',
+            connection: {
+                host     : '127.0.0.1',
+                user     : 'root',
+                password : '',
+                database : 'ghost_testing',
+                charset  : 'utf8'
+            }
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '2369'
+        },
+        logging: false
+    },
+
+    // ### Testing pg
+    // Used by Travis - Automated testing run through GitHub
+    'testing-pg': {
+        url: 'http://127.0.0.1:2369',
+        database: {
+            client: 'pg',
+            connection: {
+                host     : '127.0.0.1',
+                user     : 'postgres',
+                password : '',
+                database : 'ghost_testing',
+                charset  : 'utf8'
+            }
+        },
+        server: {
+            host: '127.0.0.1',
+            port: '2369'
+        },
+        logging: false
+    }
+};
+
+// Export config
+module.exports = config;
