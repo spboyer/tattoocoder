862528441f4fdbf2080ebd1ec970fea47d770a78
Shayne Boyer <shayne.p.boyer@disney.com>
2016-04-22 10:05:47 -0400

    add theme back

---
+++ content/themes/th-ghost-theme/assets/js/spboyer.js
@@ -0,0 +1,29 @@
+// Developer media ads
+function _dmBootstrap(file) {
+  var _dma = document.createElement('script');
+  _dma.type = 'text/javascript';
+  _dma.async = true;
+  _dma.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + file;
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(_dma);
+}
+function _dmFollowup(file) { if (typeof DMAds === 'undefined') _dmBootstrap('cdn2.DeveloperMedia.com/a.min.js'); }
+(function () { _dmBootstrap('cdn1.DeveloperMedia.com/a.min.js'); setTimeout(_dmFollowup, 2000); })();
+
+// Disqus
+var disqus_shortname = 'spboyer';
+(function () {
+    var s = document.createElement('script');
+    s.async = true;
+    s.type = 'text/javascript';
+    s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+    (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());
+
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+// set id
+ga('create', 'UA-48228610-1', 'auto');
+ga('send', 'pageview');
