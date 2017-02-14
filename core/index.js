900d40fbabbf290aa9cef9f95b2b8d77be484ef9
Shayne Boyer <spboyer@live.com>
2017-02-14 09:56:07 -0500

    add appinsights

--- core/index.js
+++ core/index.js
@@ -1,6 +1,8 @@
 // ## Server Loader
 // Passes options through the boot process to get a server instance back
 var server = require('./server');
+var appInsights = require("applicationinsights");
+appInsights.setup().start();
 
 // Set the default environment to be `development`
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
